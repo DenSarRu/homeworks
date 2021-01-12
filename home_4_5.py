3df40004f97c7cff3b6e4f0f775966222fbf4640
DenSarRu <davydov.sarov@gmail.com>
2021-01-12 19:40:25 +0300

    add homeworks for 4d lesson

---
+++ home_4_5.py
@@ -0,0 +1,13 @@
+# Реализовать формирование списка, используя функцию range() и возможности генератора.
+# В список должны войти четные числа от 100 до 1000 (включая границы).
+# Необходимо получить результат вычисления произведения всех элементов списка.
+from functools import reduce
+
+
+def my_func(prev, element):
+    return prev * element
+
+
+original_list = [elem for elem in range(100, 1001, 2)]
+print(f'Исходный список четных значений от 100 до 1000:\n{original_list}')
+print(f'Произведение всех элементов списка:\n{reduce(my_func, original_list)}')
