333a954c804e2c9e1d08a28a67357c98d7e0a45c
DenSarRu <davydov.sarov@gmail.com>
2021-01-22 08:10:46 +0300

    change some files

--- home_5_3.py
+++ home_5_3.py
@@ -8,17 +8,23 @@ with open('homework_5_3.txt', encoding='utf-8', errors='ignore') as inf, \
         if not line.isspace():
             out.write(line)
 # работаем с полученным файлом
-with open('homework_5_3_out.txt', encoding='utf-8', errors='ignore') as file:
-    lines = file.readlines()
-    salary_less_20 = []
-    average_salary = 0
-    for elem in lines:
-        if not elem.isspace():
-            if int(elem.split()[1]) < 20000:
-                salary_less_20.append(elem.split()[0])
-            average_salary += int(elem.split()[1])
-    average_salary /= len(lines)
-    print(f'Общее количество сотрудников: {len(lines)} человек.')
-    print(f'Средняя величина дохода сотрудников: {average_salary}')
-    print(f'Сотрудников с окладом менее 20 тыс.: {len(salary_less_20)} человек')
-    print('Список этих сотрудников:\n{}'.format('\n'.join(salary_less_20)))
+# with open('homework_5_3_out.txt', encoding='utf-8', errors='ignore') as file:
+#     lines = file.readlines()
+#     salary_less_20 = []
+#     average_salary = 0
+#     for elem in lines:
+#         if not elem.isspace():
+#             if int(elem.split()[1]) < 20000:
+#                 salary_less_20.append(elem.split()[0])
+#             average_salary += int(elem.split()[1])
+#     average_salary /= len(lines)
+#     print(f'Общее количество сотрудников: {len(lines)} человек.')
+#     print(f'Средняя величина дохода сотрудников: {average_salary}')
+#     print(f'Сотрудников с окладом менее 20 тыс.: {len(salary_less_20)} человек')
+#     print('Список этих сотрудников:\n{}'.format('\n'.join(salary_less_20)))
+
+
+with open('homework_5_3_out.txt', 'r', encoding='utf-8') as f:
+    employees_dict = {line.split()[0]: float(line.split()[1]) for line in f}
+    print(f'Average salary = {round(sum(employees_dict.values()) / len(employees_dict), 3)}\n'
+          f'Employees with salary less than 20k {[e[0] for e in employees_dict.items() if e[1] < 20000]}')
--- home_5_5.py
+++ home_5_5.py
@@ -41,3 +41,8 @@ while True:
 
 
 # print(sum(map(int, file.readline().split())))
+# from random import randint
+# with open('5.txt', mode='w+', encoding='utf-8') as f:
+#     f.write(" ".join([str(randint(1, 1000)) for _ in range(100000)]))
+#     f.seek(0)
+#     print(sum(map(int, f.readline().split())))
